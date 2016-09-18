d0065b1a3632b11097c1677342c21e6d970288e9
Nima Boscarino <nima@d207-023-168-254.wireless.sfu.ca>
2016-09-17 18:12:14 -0700

    post in sinatra

--- routes.rb
+++ routes.rb
@@ -1,7 +1,14 @@
 require 'sinatra'
+require 'json'
 
-set: port, 1337
+set :port, 1337
 
 get '/' do
   File.read('index.html')
+end
+
+post '/report' do
+  content_type :json
+  a = `ls`
+  { :key1 => a }.to_json
 end
