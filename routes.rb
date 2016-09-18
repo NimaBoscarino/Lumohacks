9bb036ea24f09491d94c0b8b5d2e3cd4dbfc7e67
Nima Boscarino <nima@d207-023-168-254.wireless.sfu.ca>
2016-09-17 20:37:44 -0700

    changed to accept form

--- routes.rb
+++ routes.rb
@@ -9,19 +9,9 @@ end
 
 post '/report' do
   content_type :json
-  request.body.rewind
-  request_payload = JSON.parse request.body.read
 
-  #a = `Rscript helloworld.r`
-  { :name => request_payload['name'],
-    :age => request_payload['age'],
-    :ethnicity => request_payload['ethnicity']}.to_json
-end
-
-post '/upload' do
-  File.open('uploads/' + params['myfile'][:filename], "w") do |f|
-        f.write(params['myfile'][:tempfile].read)
-          end
-    return "The file was successfully uploaded!"
+  { :name => params['name'],
+    :age => params['age'],
+    :ethnicity => params['ethnicity']}.to_json
 end
 
